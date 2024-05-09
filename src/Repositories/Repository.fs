5d6fe86ae35cf42acabb2fefd967647d3357e79c
alaza5 <wojciech3313@gmail.com>
2024-05-09 23:19:15 +0200

    tmep

--- src/Repositories/Repository.fs
+++ src/Repositories/Repository.fs
@@ -6,80 +6,70 @@ module Repository =
   open Dapper
   open Models
   open System
+  open DapperExtensions.DapperExtensions
 
+
+  let sqlInsertUser =
+    """
+      INSERT INTO users (id, email, username, password, bio, image, created_at, updated_at) 
+            OUTPUT inserted.*
+            VALUES (@id, @email, @username, @password, @bio, @image, @created_at, @updated_at)
+    """
 
   let registerUser (conn: IDbConnection) (data: DatabaseModels.users) =
-    insert {
-      into DatabaseModels.usersTable
-      value data
-    }
-    |> conn.InsertOutputAsync<DatabaseModels.users, DatabaseModels.users>
+    conn.QuerySingleAsyncResult<DatabaseModels.users>(sqlInsertUser, data)
 
+
+
+  let private sqlGetUserByEmail = "SELECT * FROM users WHERE email = @email"
 
   let fetchCurrentUser (conn: IDbConnection) (email: string) =
-    task {
-      let! users =
-        select {
-          for user in DatabaseModels.usersTable do
-            where (user.email = email)
-        }
-        |> conn.SelectAsync<DatabaseModels.users>
+    conn.QuerySingleAsyncResult<DatabaseModels.users>(sqlGetUserByEmail, {| email = email |})
 
-      return
-        match users |> Seq.tryHead with
-        | Some x -> Ok x
-        | None -> Error "User not found"
-    }
+
+
+  let private sqlUpdateUser =
+    "UPDATE users SET id = @id, email = @email, username = @username, password = @password, bio = @bio, image = @image, created_at = @created_at, updated_at = @updated_at WHERE email = @currentUserEmail"
 
   let updateUser
     (conn: IDbConnection)
     (currentUserEmail: string)
     (updatedUser: DatabaseModels.users)
     =
-    update {
-      for user in DatabaseModels.usersTable do
-        set updatedUser
-        where (user.email = currentUserEmail)
-    }
-    |> conn.UpdateOutputAsync<DatabaseModels.users, DatabaseModels.users>
+    let param =
+      {| updatedUser with
+          currentUserEmail = currentUserEmail |}
+
+    conn.QuerySingleAsyncResult<DatabaseModels.users>(sqlUpdateUser, param)
 
 
+
+  let sqlCreateArticle =
+    "INSERT INTO articles (id, author_id, slug, title, description, body, created_at, updated_at) VALUES (@id, @author_id, @slug, @title, @description, @body, @created_at, @updated_at)"
 
   let createArticle (conn: IDbConnection) (article: DatabaseModels.articles) =
-    task {
-      let! retVal =
-        insert {
-          into DatabaseModels.articlesTable
-          value article
-        }
-        |> conn.InsertOutputAsync<DatabaseModels.articles, DatabaseModels.articles>
+    conn.QuerySingleAsyncResult<DatabaseModels.articles>(sqlUpdateUser, article)
 
-      return
-        match retVal |> Seq.tryHead with
-        | None -> Error "Did't inserted article"
-        | Some x -> Ok x
-    }
 
+
+  let sqlAddTags =
+    @"INSERT INTO tags (id, name) VALUES (@id, @name) ON CONFLICT (name) DO NOTHING;"
 
   let addTags (conn: IDbConnection) (tagsRequest: AddTagsRequest) =
-    let sql =
-      @"INSERT INTO tags (id, name) VALUES (@id, @name) ON CONFLICT (name) DO NOTHING;"
-
     let listOfTags =
       tagsRequest.tags |> List.map (fun tag -> {| id = Guid.NewGuid(); name = tag |})
 
-    conn.Execute(sql, listOfTags)
+    conn.QuerySingleAsyncResult<DatabaseModels.tags>(sqlUpdateUser, listOfTags)
+
+
+  let sqlGetTags = "SELECT * FROM tags"
 
   let getTags (conn: IDbConnection) =
-    select {
-      for tags in DatabaseModels.tagsTable do
-        selectAll
-    }
-    |> conn.SelectAsync<DatabaseModels.tags>
+    conn.QueryAsyncResult<DatabaseModels.tags>(sqlGetTags)
+
+
+  let sqlFindInTags = sprintf "SELECT * FROM tags WHERE name IN @tagsToFind"
 
   let findTags (conn: IDbConnection) (tagsToFind: string list) =
-    select {
-      for tags in DatabaseModels.tagsTable do
-        where (isIn tags.name tagsToFind)
-    }
-    |> conn.SelectAsync<DatabaseModels.tags>
+    let param = tagsToFind |> List.toArray
+    conn.QueryAsync<DatabaseModels.tags>({| tagsToFind = param |})
